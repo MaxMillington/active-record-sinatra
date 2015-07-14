70a5a878739512b8819152a82b6e3288e300622e
MaxMillington <maxmillington89@gmail.com>
2015-07-13 20:11:13 -0600

    finished ActiveRecord homework

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,7 +1,11 @@
 class TaskManager < Sinatra::Base
   get '/tasks' do
-    # @tasks = Task.all
-    @users = User.all
+    @tasks = Task.all
     erb :tasks_index
+  end
+
+  get '/users' do
+    @users = User.all
+    erb :users_index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-x``# encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,11 @@ x``# encoding: UTF-8
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713204555) do
+ActiveRecord::Schema.define(version: 20150714015036) do
+
+  create_table "statuses", force: :cascade do |t|
+    t.text "name"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.text     "title"
@@ -19,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150713204555) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "status_id"
   end
 
   create_table "users", force: :cascade do |t|
